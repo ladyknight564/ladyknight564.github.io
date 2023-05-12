7a5a7bf8a56a14b678b76daa5befd5cb4802d843
ladyknight564 <kaperkins5010@gmail.com>
2023-05-12 18:56:05 -0500

    wrote the javascript function to send the email and added the link to the file to the page

--- java/contactform.js
+++ java/contactform.js
@@ -1,1 +1,11 @@
 // form submit: to send an email to me
+function sendMail() {
+    var link = "mailto:ladyknight564@hotmail.com?cc="
+    + encodeURI(document.getElementById('mEmail').value) + "&subject=" + encodeURI("Hello my name is \ ")
+    + encodeURI(document.getElementById('MessengerName'))
+    + encodeURI("\  and I want to talk to you about \ ") + encodeURI(document.getElementById('mSubject').value)
+    + "&body=" + encodeURI(document.getElementById('Message').value);
+    console.log(link);
+    window.location.href = link;
+
+}
